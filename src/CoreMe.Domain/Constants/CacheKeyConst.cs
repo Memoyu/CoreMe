8237f10662ee5d24f9c1c0831c6ecff0fba00030
Memoyu <mmy6076@outlook.com>
2024-07-11 11:23:57 +0800

    🔧 调整配置 以及 完善文档

--- src/CoreMe.Domain/Constants/CacheKeyConst.cs
+++ src/CoreMe.Domain/Constants/CacheKeyConst.cs
@@ -1,5 +1,4 @@
 ﻿namespace CoreMe.Domain.Constants;
 public static class CacheKeyConst
 {
-    public static string ArticleView(long articleId, long visitorId) => $"memo-blog:article:views:{articleId}:{visitorId}";
 }
